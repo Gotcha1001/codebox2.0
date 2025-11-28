9a6b58444520a516ccb61cb0a7d326a8c88db9fe
Wesley <WesleyOlivier443@gmail.com>
2025-11-28 09:26:26 +0200

    Initial

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className=" flex min-h-screen justify-center">
+      <h1 className="font-game gradient-title text-5xl"> Welcome back</h1>
+    </div>
+  );
+}
